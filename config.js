a8a81a6732aa59617f8ca9e369fdce3006a43e93
Matt Star <ms.starakiewicz@gmail.com>
2019-05-10 01:22:43 +0200

    Add google api and set real sign in

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '355514537879-g9fnhi0410fmr2hpcb2bbh07p4vco5o2.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'BqUFKotneuq8TibrbJ0OtwQB',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
